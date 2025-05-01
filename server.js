3c49dd4cd5f0d2fb50bd069f47b7d06b9f96be03
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-05-01 23:56:44 +0800

    fix: 更新 CORS 設定以允許來自測試來源的請求

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 8700
 
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ limit: '2mb', extended: true }));
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.static('dist'))
 
 app.use(session({
