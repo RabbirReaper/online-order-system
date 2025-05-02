dc49b27635b0869d3fe52ee32a36243658196433
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-05-03 01:29:59 +0800

    fix: 取消註解餐點實例路由，恢復相關功能

--- server/routes/dish.js
+++ server/routes/dish.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import * as dishTemplateController from '../controllers/Dish/dishTemplate.js';
-import * as dishInstanceController from '../controllers/Dish/dishInstance.js';
+// import * as dishInstanceController from '../controllers/Dish/dishInstance.js';
 import * as optionCategoryController from '../controllers/Dish/optionCategory.js';
 import * as optionController from '../controllers/Dish/option.js';
 import { authMiddleware } from '../middlewares/auth.js';
@@ -17,11 +17,11 @@ router.delete('/templates/:id', authMiddleware, roleMiddleware(['boss', 'brand_a
 router.get('/templates/:id/options', authMiddleware, brandMiddleware, dishTemplateController.getDishTemplateOptions);
 
 // 餐點實例路由
-router.get('/instances', dishInstanceController.getAllDishInstances);
-router.get('/instances/:id', dishInstanceController.getDishInstanceById);
-router.post('/instances', dishInstanceController.createDishInstance);
-router.put('/instances/:id', dishInstanceController.updateDishInstance);
-router.delete('/instances/:id', authMiddleware, permissionMiddleware(['edit_backend']), dishInstanceController.deleteDishInstance);
+// router.get('/instances', dishInstanceController.getAllDishInstances);
+// router.get('/instances/:id', dishInstanceController.getDishInstanceById);
+// router.post('/instances', dishInstanceController.createDishInstance);
+// router.put('/instances/:id', dishInstanceController.updateDishInstance);
+// router.delete('/instances/:id', authMiddleware, permissionMiddleware(['edit_backend']), dishInstanceController.deleteDishInstance);
 
 // 選項類別路由 (主資料庫，由品牌管理員或老闆管理)
 router.get('/option-categories', authMiddleware, brandMiddleware, optionCategoryController.getAllOptionCategories);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import authRoutes from './auth.js';
-// import dishRoutes from './dish.js';
+import dishRoutes from './dish.js';
 import storeRoutes from './store.js';
 // import orderRoutes from './order.js';
 // import promotionRoutes from './promotion.js';
@@ -12,7 +12,7 @@ const apiRouter = express.Router();
 
 // 將所有路由掛載到 API 路由器上
 apiRouter.use('/auth', authRoutes);
-// apiRouter.use('/dish', dishRoutes);
+apiRouter.use('/dish', dishRoutes);
 apiRouter.use('/store', storeRoutes);
 // apiRouter.use('/order', orderRoutes);
 // apiRouter.use('/promotion', promotionRoutes);
