c707985a62d1b9fbdf662f8cb44ce37314aa91bf
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-05-08 23:58:19 +0800

    feat(dependencies): 新增 bootstrap-vue-next 依賴以支援 BootstrapVue3

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import BootstrapVue3 from 'bootstrap-vue-3'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+// Bootstrap and BootstrapVue3 CSS files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import App from './App.vue'
@@ -12,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(BootstrapVue3) // 使用 BootstrapVue3
 
 app.mount('#app')
