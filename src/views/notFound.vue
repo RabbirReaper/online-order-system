12020f3b39016f6717a33d8d13cc49fc83ed13cc
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-05-18 22:02:32 +0800

    fix: 注釋掉返回首頁的鏈接以清理404頁面內容

--- src/views/notFound.vue
+++ src/views/notFound.vue
@@ -2,7 +2,7 @@
   <div class="not-found">
     <h1>404 - Page Not Found</h1>
     <p>Sorry, the page you are looking for does not exist.</p>
-    <router-link to="/">Go back to Home</router-link>
+    <!-- <router-link to="/">Go back to Home</router-link> -->
   </div>
 </template>
 
