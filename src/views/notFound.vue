83bfe23d86283ba4c7c4dc7a36fcf046bd7f0a41
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-05-25 00:28:04 +0800

    feat: 在404頁面中新增提示信息，指導用戶重新掃碼店家QR code

--- src/views/notFound.vue
+++ src/views/notFound.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="not-found">
     <h1>404 - Page Not Found</h1>
+    <h2>請重新掃碼店家QR code</h2>
     <p>Sorry, the page you are looking for does not exist.</p>
     <!-- <router-link to="/">Go back to Home</router-link> -->
   </div>
