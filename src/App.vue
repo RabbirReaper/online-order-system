d84081576639a040e76bd4f5144e27b7f81da83b
Rabbir_Reaper <abc0979999412@gmail.com>
2025-08-12 23:19:54 +0800

    feat(App): 新增緩存最大上限

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <router-view v-slot="{ Component, route }">
-    <keep-alive :include="keepAliveList">
+    <keep-alive :include="keepAliveList" :max="maxCacheCount">
       <component :is="Component" :key="route.fullPath" />
     </keep-alive>
   </router-view>
@@ -11,4 +11,5 @@ import { ref } from 'vue'
 
 // 需要緩存的組件名稱列表
 const keepAliveList = ref(['MenuView'])
+const maxCacheCount = ref(3)
 </script>
