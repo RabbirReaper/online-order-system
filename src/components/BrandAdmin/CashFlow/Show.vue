f8ab2ae4784dc3b5832aa9eeed55b8a12bcde248
Rabbir_Reaper <abc0979999412@gmail.com>
2025-10-06 16:58:51 +0800

    delete(store): 記帳系統只需要顯示日期

--- src/components/BrandAdmin/CashFlow/Show.vue
+++ src/components/BrandAdmin/CashFlow/Show.vue
@@ -407,9 +407,6 @@ const formatDate = (dateStr) => {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    timeZone: 'Asia/Taipei',
   })
 }
 
