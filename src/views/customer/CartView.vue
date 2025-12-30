0db4f7af429747ed7f10e045d41f3dc9218830f2
Rabbir_Reaper <rabbirreaper@gmail.com>
2025-12-30 14:38:37 +0800

    style: 微調顯示文字

--- src/views/customer/CartView.vue
+++ src/views/customer/CartView.vue
@@ -252,7 +252,7 @@
       >
         <div class="container-button" style="max-width: 540px">
           <button class="btn w-100 py-2 checkout-btn" @click="checkout" :disabled="!isFormValid">
-            前往結帳 - ${{ calculateTotal() }}
+            前往結帳   ${{ calculateTotal() }}
           </button>
         </div>
       </div>
