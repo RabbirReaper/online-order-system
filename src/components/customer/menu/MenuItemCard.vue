db8d7737803d4ffc63f8b533a15b69b78518cdfa
Rabbir_Reaper <abc0979999412@gmail.com>
2026-01-03 11:05:40 +0800

    style(customer): 微調價格顏色

--- src/components/customer/menu/MenuItemCard.vue
+++ src/components/customer/menu/MenuItemCard.vue
@@ -432,8 +432,8 @@ const handleClick = () => {
 
 .item-price-tag {
   display: inline-block;
-  background-color: #d3540049;
-  color: #d35400;
+  background-color: #d3540034;
+  color: #d35400ee;
   font-weight: 700;
   font-size: 1.1rem;
   padding: 0.3rem 0.8rem;
